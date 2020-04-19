a9225f547d5bde76eed7b394938d300d5c0d5ffc
Keyno <kofujiw@gmail.com>
2020-04-19 22:48:18 +0900

    phpunit sample

---
+++ tests/SampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+Class SampleTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function 試しに実行してみる()
+    {
+        $this->assertTrue(true);
+    }
+}
