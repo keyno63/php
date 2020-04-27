28712fea8231e840834eaf0c1652467b10187079
Keyno <kofujiw@gmail.com>
2020-04-28 01:04:53 +0900

    add s3 client

---
+++ src/bin/aws_s3.php
@@ -0,0 +1,38 @@
+#!/bin/env php
+<?php
+set_include_path("/opt/project");
+
+require '/vendor/autoload.php';
+
+
+use Aws\S3\S3Client;
+use Aws\Exception\AwsException;
+
+$key = $argv[1];
+$secret = $argv[2];
+
+$s3Client = S3Client::factory([
+    'credentials' => [
+        'key' => $key,
+        'secret' => $secret
+    ],
+    'region' => 'us-east-2', //リージョンは日本
+    'version' => 'latest' //バケットのバージョニングは最新
+]);
+
+$bucketName = "kofujiw-s3bucket";
+$key = "sample1.txt";
+$content = $s3Client->getObject(
+    [
+        'Bucket' => $bucketName,
+        'Key'    => $key,
+    ]
+);
+$content['Body']->rewind();
+
+$fileName = "/tmp/file.txt";
+
+// ファイルの保存
+$fp = fopen($fileName, 'w');
+fputs($fp, $content['Body']->read($content['ContentLength']));
+fclose($fp);
